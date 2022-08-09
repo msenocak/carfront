7902de40bd86730d9029c6e594dd57d124415e0c
msenocak <msenocak3@gmail.com>
2022-08-08 19:53:50 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 //export const SERVER_URL='http://localhost:8080/';
-//export const SERVER_URL='https://mscarbackend.herokuapp.com/';
-export const SERVER_URL='https://murmuring-coast-45229.herokuapp.com/';
+export const SERVER_URL='https://mscarbackend.herokuapp.com/';
 //export const SERVER_URL='http://ch154cardatabasemariadb2-env.eba-xmx9ztsz.us-west-1.elasticbeanstalk.com/';
 //export const SERVER_URL='https://ch154cardatabaseh2-env.eba-smhjkhm5.us-west-1.elasticbeanstalk.com/';
